34f1d3a11c3d4f8364fd1358c5b3ea96b35a7376
Jose Jimenez <josejimenez826@gmail.com>
2021-10-17 22:47:17 -0700

    got navbar to work and need to style and add more to it

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
+import { FiMenu } from "react-icons/fi"
+
 function Navbar() {
+    const [navStyle, setNavStyle] = useState({display: "none"});
+    const [navBack, setNavBack] = useState({backgroundColor: "transparent"});
+
+    const toggleNav = () => {
+        if(navStyle.display === "none"){
+            setNavStyle({display: "flex"});
+            setNavBack({backgroundColor: "#444"});
+        } else {
+            setNavStyle({display: "none"});
+            setNavBack({backgroundColor: "transparent"});
+        }
+    }
+
     return (
-        <div id="navbar">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>News</li>
-            </ul>
+        <div id="navbar" style={navBack}>
+            <div id="navIcon">
+                <button id="icon" onClick={toggleNav}><FiMenu size={42} color="white" /></button>
+            </div>
+            <div id="navBtns" style={navStyle}>
+                <button className="navBtn" onClick={toggleNav}><a href="/">Home</a></button>
+            </div>
         </div>
     )
 }
