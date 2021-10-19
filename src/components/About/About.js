b7e418c946bff2b3a245c3983e7c630c16606fc5
Jose Jimenez <josejimenez826@gmail.com>
2021-10-19 15:34:37 -0700

    added the about section need to make better description

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function About() {
+    return (
+        <div id="about">
+            <p>Easily search up different recipes and enjoy looking through a verity of foods and dishes. </p> 
+        </div>
+    )
+}
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./main.css";
 
 import Header from "./../components/Header/Header";
+import About from "./../components/About/About";
 
 function Home() {
     return (
         <div>
             <Header />
+            <About />
         </div>
     )
 }
