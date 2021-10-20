aeb6e63268a2d04c91df4db2d5c513d370b6ab02
Jose Jimenez <josejimenez826@gmail.com>
2021-10-19 22:46:17 -0700

    change nav to fix the way it shows up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,27 +5,37 @@ import { FiMenu } from "react-icons/fi"
 import { BsFileX } from "react-icons/bs";
 
 function Navbar() {
-    const [navStyle, setNavStyle] = useState({display: "none"});
+    const [navStyle, setNavStyle] = useState({display: "none", zIndex: -2});
     const [navBack, setNavBack] = useState({backgroundColor: "transparent"});
     const [navHeight, setNavHeight] = useState({height: "10vh"});
 
     const toggleNav = () => {
         if(navStyle.display === "none"){
-            setNavStyle({display: "flex"});
+            setNavStyle({display: "flex", zIndex: 3});
             setNavBack({backgroundColor: "#444"});
-            // setNavBack({backgroundColor: "#444", display: "flex"});
-            // setNavHeight({height: "10vh", width: "auto"})
         } else {
-            setNavStyle({display: "none"});
-            setNavBack({backgroundColor: "transparent", display: "none"});
-            // setNavHeight({height: "100vh", width: "100%"})
+            setNavStyle({display: "none", height: 0, zIndex: -2});
+            setNavBack({backgroundColor: "transparent"});
         }
     }
 
     return (
         <div id="navbar"> 
-        {/* style={navBack}> */}
-            <div id="navMain" style={navBack}>
+            <div id="navIcon">
+                <button id="icon" onClick={toggleNav}><FiMenu size={42} color="white" /></button>
+            </div>
+
+            <div id="navItems" style={navStyle}>
+                {/* <div id="navGroup" style={navStyle}> */}
+                    <div id="navBtns">
+                        <button className="navBtn" onClick={toggleNav}><a href="/">Home</a></button>
+                    </div>
+                    
+                    <button id="dim" onClick={toggleNav} style={navStyle}>
+                    </button>
+                {/* </div> */}
+            </div>
+            {/* <div id="navMain" style={navBack}>
                 <div id="navIcon">
                     <button id="icon" onClick={toggleNav}><FiMenu size={42} color="white" /></button>
                 </div>
@@ -33,10 +43,11 @@ function Navbar() {
                     <button className="navBtn" onClick={toggleNav}><a href="/">Home</a></button>
                 </div>
             </div>
+
             <div id="navDim" style={navStyle}>
                 <button id="dim" onClick={toggleNav}>
                 </button>
-            </div>
+            </div> */}
             
         </div>
     )
